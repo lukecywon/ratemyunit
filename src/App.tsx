f6116b13954bf116bb9291418233554509f99eb2
freshflick <shayannadeem1x@gmail.com>
2025-07-21 23:47:45 +0400

    Added sign up/log in button + description

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import rateMyUnitLogo from './assets/logo-default.svg'
 import './styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={rateMyUnitLogo} className="logo" alt="Rate My Unit logo" />
-        </a>
-      </div>
-      <h1>Welcome to Rate My Unit</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+    return (
+      <>
+          <div>
+              <a href="https://react.dev" target="_blank">
+                  <img src={rateMyUnitLogo} className="logo" alt="Rate My Unit logo"/>
+              </a>
+          </div>
+          <br/>
+          <b>
+              <h1>Welcome to Rate My Unit</h1>
+          </b>
+          <div className="card flex justify-center gap-10">
+              <button onClick={() => {}}>
+                  Sign up
+              </button>
+              <button onClick={() => {}}>
+                  Log in
+              </button>
+          </div>
+          <div>
+              <p>
+                  Explore honest, student-written reviews for Monash University units.
+                  Whether you're planning your next semester or curious about a subject,
+                  our community-driven insights will help you make informed decisions.
+              </p>
+              <br/>
+              <p>
+                  Sign up to share your own experiences, or browse reviews to see what others thought.
+                  Let’s make unit selection a little easier—together.
+              </p>
+          </div>
+      </>
   )
 }
 
