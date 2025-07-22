7c5ef93c4e0d5261be8e39ed873a24e139e35867
Luke Won <lukecywon@gmail.com>
2025-07-23 01:56:20 +0800

    Updated Landing Page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,22 +1,21 @@
-import rateMyUnitLogo from "../assets/logo-white-nobackground.svg";
+import rateMyUnitLogo from "../assets/logo-white-fancy.svg";
 import Typewriter from "../components/Typewriter.tsx";
 
 function Home() {
     return (
         <>
             <div className="h-screen">
-                <div className="flex flex-row h-full items-center">
-                    <div className="basis-2/3 h-full">
-                        test
+                <div className="flex flex-row h-full items-center bg-linear-to-r from-[#0f0c29] via-[#302b63] to-[#24243e]">
+                    <div className="flex relative justify-center items-center basis-1/2 h-full p-10">
+                        <img src={rateMyUnitLogo} className="w-2/3 sm:w-1/2 md:w-1/3 lg:w-3/5 h-auto" alt="Rate My Unit logo"/>
+                        <Typewriter text="ratemyunit" speed={300} className="font-mono text-xl sm:text-2xl md:text-4xl lg:text-6xl text-white relative top-20" />
                     </div>
-                    <div className="flex flex-col items-center justify-center basis-1/3 h-full bg-black p-10">
-                        <img src={rateMyUnitLogo} className="size-max" alt="Rate My Unit logo"/>
-                        <Typewriter text="ratemyunit" speed={200} className="font-mono text-7xl text-white" />
+                    <div className="basis-1/2 h-full">
+
                     </div>
                 </div>
             </div>
         </>
     )
 }
-
 export default Home;
