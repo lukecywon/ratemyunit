596c21a054f83d8c6feee8f874a04f8929c9e11e
Luke Won <lukecywon@gmail.com>
2025-07-22 22:27:52 +0800

    Changes to attempt to fix double rendering bug

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Routes>
             <Route path="/">
                 <Route index element={<App />}></Route>
