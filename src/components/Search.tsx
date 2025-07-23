1b1f97f1d345d62956eb3092e64467c63eb4d204
freshflick <shayannadeem1x@gmail.com>
2025-07-23 22:34:53 +0400

    fixed resizing issue for search bar, moved search icon to improvise for clear all button

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -22,15 +22,16 @@ const SearchBar: React.FC<SearchBarProps> = ({placeholder = 'Search units or rev
             <div className="relative w-full">
                 <input
                     placeholder={ placeholder }
-                    className="input shadow-lg focus:border-2 border-gray-300 px-5 py-3 rounded-xl w-full transition-all focus:w-full outline-none h-20 text-2xl bg-white/70 hover:bg-white focus:bg-white text-gray-500 font-mono"
+                    className="input shadow-lg focus:border-2 border-gray-300 px-4 py-3 rounded-xl w-full transition-all focus:w-full outline-none
+                    h-16 max-w-full sm:h-18 sm:max-w-full text-xl sm:text-2xl bg-white/70 hover:bg-white focus:bg-white text-gray-500 font-mono"
                     name="search"
                     type="search"
                     onChange={ handleChange }
                     value={ query }
                 />
-                <button type="submit" className="">
+                <button type="submit" className="absolute top-1/2 -translate-y-1/2 p-2 text-gray-500 hover:text-gray-700">
                     <svg
-                        className="size-6 absolute top-7 right-6 text-gray-500"
+                        className="w-6 h-6"
                         stroke="currentColor"
                         stroke-width="1.5"
                         viewBox="0 0 24 24"
@@ -41,7 +42,7 @@ const SearchBar: React.FC<SearchBarProps> = ({placeholder = 'Search units or rev
                             d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
                             stroke-linejoin="round"
                             stroke-linecap="round"
-                        ></path>
+                        />
                     </svg>
                 </button>
             </div>
