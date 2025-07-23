62d51a9d85f28e644c89d14d3b08dd544ea74d6c
Luke Won <lukecywon@gmail.com>
2025-07-23 19:23:55 +0800

    Added padding and changed font of description

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -15,9 +15,9 @@ function Home() {
                 <div className="background flex flex-col lg:flex-row h-full items-center justify-center">
                     <div className="flex relative justify-center items-center basis-1/2 h-full p-3 gap-2 pr-20">
                         <img src={rateMyUnitLogo} className="w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-60 lg:h-60 p-3" alt="Rate My Unit logo"/>
-                        <div className="flex flex-col gap-4">
+                        <div className="flex flex-col gap-4 p-10">
                             <Typewriter text="ratemyunit" speed={350} className="font-mono text-3xl sm:text-4xl md:text-4xl lg:text-5xl text-black relative" />
-                            <div className="description mt-2">
+                            <div className="font-mono mt-4">
                                 <p>
                                     Explore honest, student-written reviews for Monash University units.
                                     Whether you're planning your next semester or curious about a subject,
